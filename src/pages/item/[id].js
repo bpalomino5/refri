bb066ba9a1539378a585a02000a81fd895a3bdcd
Brandon Palomino <12bpalomino@gmail.com>
2021-07-18 22:26:27 -0700

    styled item page

--- src/pages/item/[id].js
+++ src/pages/item/[id].js
@@ -3,6 +3,20 @@ import { useQuery, useQueryClient } from 'react-query';
 import firebase from '../../lib/firebase';
 const firestore = firebase.firestore();
 
+// Components
+import {
+  Container,
+  Heading,
+  HStack,
+  Button,
+  Table,
+  Thead,
+  Th,
+  Tr,
+  Td,
+  Tbody,
+} from '@chakra-ui/react';
+
 // Hooks
 import { useRouter } from 'next/router';
 import useFood from '../../hooks/use-food';
@@ -14,16 +28,14 @@ const FoodItem = () => {
   const queryClient = useQueryClient();
   const foodQuery = useFood();
 
-  const incQuantity = () => {
-    const item = foodQuery.data[category].options[option];
+  const item = foodQuery.data[category].options[option];
 
+  const incQuantity = () => {
     item.quantity += 1;
     queryClient.setQueryData('food', foodQuery.data);
   };
 
   const decQuantity = () => {
-    const item = foodQuery.data[category].options[option];
-
     if (item.quantity > 0) {
       item.quantity -= 1;
       queryClient.setQueryData('food', foodQuery.data);
@@ -32,7 +44,6 @@ const FoodItem = () => {
 
   const updateItem = async () => {
     const docId = foodQuery.data[category].category;
-    const item = foodQuery.data[category].options[option];
 
     await firestore
       .collection('categories')
@@ -48,21 +59,33 @@ const FoodItem = () => {
   }
 
   return (
-    <div style={{ margin: 25 }}>
-      <p>Item: {id}</p>
-      <pre>
-        {JSON.stringify(foodQuery.data[category].options[option], null, 2)}
-      </pre>
-      <button type="button" onClick={incQuantity}>
-        +
-      </button>
-      <button type="button" onClick={decQuantity}>
-        -
-      </button>
-      <button type="button" onClick={updateItem}>
-        Save
-      </button>
-    </div>
+    <Container centerContent>
+      <Heading as="h1" sx={{ my: 4 }}>
+        {id}
+      </Heading>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Properties</Th>
+            <Th />
+          </Tr>
+        </Thead>
+        <Tbody>
+          {Object.keys(item).map((k) => (
+            <Tr>
+              <Td>{k}</Td>
+              <Td>{item[k]}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+
+      <HStack sx={{ mt: 4 }}>
+        <Button onClick={incQuantity}>+</Button>
+        <Button onClick={decQuantity}>-</Button>
+        <Button onClick={updateItem}>Save</Button>
+      </HStack>
+    </Container>
   );
 };
 
